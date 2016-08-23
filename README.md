3f542ec2025bf0c74d3c20715b47a15e069b988d
vijaymathew <mathew.vijay@gmail.com>
2016-08-23 21:37:59 +0530

    timeouts. actors made first-class. single-arg function as message handler.

--- README.md
+++ README.md
@@ -4,20 +4,24 @@ A tiny Clojure library for concurrent computing with actors and asynchronous mes
 
 ```clojure
 (use '[cljact.core])
+(require '[clojure.core.match :refer [match]])
 
-(defact a1
+(defact subtractor
   "Receive a message from `sender` and send back the difference of `x` and `y`."
-  [sender x y] (sender [:result (- x y)]))
+  (fn [[sender x y]] (sender [(- x y)])))
 
-(defact a2
+(defact subtractor-client
   "Send two numbers to `a1`. Get back the result in another message and print it."
-  [:sub x y] (a1 [@self x y])
-  [:result x] (println x))
+  (fn [msg]
+    (println @self)
+    (match msg
+      [x y] (subtractor [@self x y])
+      [x] (println x))))
 
-(a2 [:sub 100 20])
+(subtractor-client [100 20])
 ;;-> 80
 
-(a2 [:sub 100 200])
+(subtractor-client [100 200])
 ;;-> -100
 ```
 
@@ -29,19 +33,66 @@ A tiny Clojure library for concurrent computing with actors and asynchronous mes
 
 (defact calculator
   "Dispatch messages to the `calc` multimethods."
-  calc)
+  #(calc %))
 
-(defact prxy
+(defact calculator-client
   "A proxy for `calculator`."
-  [:add x y] (calculator {:sender @self :rator :add :rands [x y]})
-  [:sub x y] (calculator {:sender @self :rator :sub :rands [x y]})
-  result (println result))
+  (fn [msg]
+    (match msg
+      [:add x y] (calculator {:sender @self :rator :add :rands [x y]})
+      [:sub x y] (calculator {:sender @self :rator :sub :rands [x y]})
+      result (println result))))
 
-(prxy [:add 10 100])
+(calculator-client [:add 10 100])
 ;;-> 110
 
-(prxy [:sub 20 30])
+(calculator-client [:sub 20 30])
 ;;-> -10
+```
+
+```clojure
+;; An actor can trigger a timeout handler if no messages are received
+;; for a set duration. Durations are specified with millisecond precision.
+(defact t1
+  "Echo the message to stdout, print an alert on timeout."
+  {:timeout {:after 5000 :do (println "timeout in t1!")}}
+  #(println %))
+;; If no message if send to `t1` in 5 seconds, the message "timeout!" will be printed.
+
+;; By default, the timeout handler is rescheduled. To prevent this, set the loop? option
+;; to false.
+(defact t2
+  "Echo the message to stdout, print an alert on timeout."
+  {:timeout {:after 5000 :do (println "timeout in t2!") :loop? false}}
+  #(println %))
+
+;; Every actor gets a function `quit` that it can call to stop itself
+;; from receiving messages and timeout events.
+(defact clock
+  "Print current time. Send the message [:stop] to stop the clock."
+  {:timeout {:after 1000 :do (println (System/currentTimeMillis))}}
+  #(when (= % :stop) (quit)))
+```
+
+```clojure
+;; Actors are first-class and are created using the `act` special form. The atom `self` gets bound to the
+;; actor object.
+(defact adder
+  "Add two numbers and send the result back to the client actor."
+  #(match % [from [x y]] (from {:result (+ x y)})))
+
+(def adder-client
+  "A client for adder. Stores the result in a local `state` variable."
+  (let [state (atom nil)]
+    (act
+      #(match %
+         [x y] (adder [@self [x y]])
+         {:result x} (reset! state x)
+         [:get] (println @state)))))
+
+(adder-client [10 20])
+(adder-client [:get])
+;;-> 30
 ```
 
 ```clojure
--- src/cljact/core.clj
+++ src/cljact/core.clj
@@ -13,35 +13,54 @@
   [expr]
   `(.submit pool (fn [] ~expr)))
 
+(defmacro act
+  [& pat-disp]
+  (let [options (when (map? (first pat-disp)) (first pat-disp))
+        msg-handler (if options (rest pat-disp) pat-disp)
+        [timeout-ms timeout-handler reschedule-timer?]
+        (when options (let [to (:timeout options)]
+                        [(:after to) (:do to) (if (contains? to :loop?)
+                                                (:loop? to)
+                                                true)]))]
+    `(let [msg-q# (ConcurrentLinkedQueue.)
+           ~(symbol 'self) (atom nil)
+           running# (atom true)
+           ~(symbol 'quit) (fn [] (reset! running# false))
+           curr-time-ms# (atom (when ~timeout-ms (System/currentTimeMillis)))
+           handler# (fn [cont#]
+                      (when @curr-time-ms#
+                        (reset! curr-time-ms# (System/currentTimeMillis)))
+                      (if-let [m# (.poll msg-q#)]
+                        (do (~@msg-handler m#)
+                            (when (and @running# (.peek msg-q#)) (schedule (cont# cont#))))
+                        (Thread/yield)))
+           to-handler# (when ~timeout-ms
+                         (fn [cont#]
+                           (when-let [old# @curr-time-ms#]
+                             (if (>= (- (System/currentTimeMillis) old#) ~timeout-ms)
+                               (do ~timeout-handler
+                                   (when ~reschedule-timer?
+                                     (reset! curr-time-ms# (System/currentTimeMillis))
+                                     (when @running# (schedule (cont# cont#)))))
+                               (when @running# (schedule (cont# cont#)))))))
+           adder# (fn [msg#]
+                    (let [schd# (.peek msg-q#)]
+                      (.add msg-q# msg#)
+                      (when-not schd#
+                        (when @running# (schedule (handler# handler#))))
+                      msg#))]
+       (reset! ~(symbol 'self) adder#)
+       (when to-handler#
+         (schedule (to-handler# to-handler#)))
+       adder#)))
+
 (defmacro defact
-  [name & pat-disp]
-  (let [docstr (if (string? (first pat-disp)) (first pat-disp) "")
-        pat-disp (if (seq docstr) (rest pat-disp) pat-disp)
-        tl (when (> (count pat-disp) 1)
-             (loop [pds pat-disp, tl []]
-               (if (seq pds)
-                 (recur (nthrest pds 2)
-                   (conj tl (first pds) `(schedule ~(second pds))))
-                 tl)))
-        multi? (not tl)
-        hd (if (symbol? (first pat-disp)) (first pat-disp) 'identity)]
-    `(def ~(symbol name) ~docstr
-       (let [msg-q# (ConcurrentLinkedQueue.)
-             ~(symbol 'self) (atom nil)
-             callback# (if ~multi?
-                         (fn [m2#] (~(symbol hd) m2#))
-                         (fn [m3#] (match m3# ~@tl)))
-             handler# (fn [cont#]
-                        (if-let [m# (.poll msg-q#)]
-                          (do (callback# m#)
-                              (when (.peek msg-q#) (schedule (cont# cont#))))
-                          (Thread/yield)))
-             adder# (fn [msg#] (let [schd# (.peek msg-q#)]
-                                 (.add msg-q# msg#)
-                                 (when-not schd# (schedule (handler# handler#)))
-                                 nil))]
-         (reset! ~(symbol 'self) adder#)
-         adder#))))
+  [name & defs]
+  (let [docstr (if (string? (first defs))
+                 (first defs)
+                 "")
+        defs (if (seq docstr) (rest defs) defs)]
+    `(def ~(symbol name) ~docstr (act ~@defs))))
 
 (defn shutdown
   []
