9872c2795463f16dc9e0a7096730c2f59aee5d9e
vijaymathew <mathew.vijay@gmail.com>
2016-08-25 21:13:31 +0530

    sync, calls with future on actors, links, customizable scheduler.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-A tiny Clojure library for concurrent computing with actors and asynchronous messaging.
+A tiny Clojure library for concurrent computing with light-weight actors and asynchronous messaging.
 
 ## Example Usage
 
@@ -49,8 +49,29 @@ A tiny Clojure library for concurrent computing with actors and asynchronous mes
 ```
 
 ```clojure
-;; An actor can trigger a timeout handler if no messages are received
-;; for a set duration. Durations are specified with millisecond precision.
+;; Actors are first-class and are created using the `act` special form. The atom `self` gets bound to the
+;; current actor object.
+(defact adder
+  "Add two numbers and send the result back to the client actor."
+  #(match % [from [x y]] (from {:result (+ x y)})))
+
+(def adder-client
+  "A client for adder. Stores the result in a local `state` variable."
+  (let [state (atom nil)]
+    (act
+      #(match %
+         [x y] (adder [@self [x y]])
+         {:result x} (reset! state x)
+         [:get] (println @state)))))
+
+(adder-client [10 20])
+(adder-client [:get])
+;;-> 30
+```
+
+```clojure
+;; An actor can trigger a timeout handler if no message is received
+;; within a specific time period. Durations are specified with millisecond precision.
 ;; In the following program, if no message is send to the actor `t1` within 5 seconds, 
 ;; a textual warning is printed.
 (defact t1
@@ -74,27 +95,75 @@ A tiny Clojure library for concurrent computing with actors and asynchronous mes
 ```
 
 ```clojure
-;; Actors are first-class and are created using the `act` special form. The atom `self` gets bound to the
-;; current actor object.
-(defact adder
-  "Add two numbers and send the result back to the client actor."
-  #(match % [from [x y]] (from {:result (+ x y)})))
+;; An actor can receive four special messages `:sync`, `:async`, `:linker` and `:quit`.
+;;
+;; `:sync` returns a function that invoke the message handler in a blocking call.
+;;
+;; `:async` returns a function that invoke the message handler asynchronously. A `future`
+;; is returned that can later return the result of the computation.
+;;
+;; `:quit` will remove the actor from the scheduler so it stops responding to all
+;; asynchronous messages.
+;;
+;; `:linker` returns a function that can be called to link and unlink other actors to
+;; monitor the status of this actor.
 
-(def adder-client
-  "A client for adder. Stores the result in a local `state` variable."
-  (let [state (atom nil)]
+;; Following program demonstrates `:sync`, `:async` and `:quit`.
+(def counter
+  "An actor with changing state."
+  (let [count (atom 0)]
     (act
-      #(match %
-         [x y] (adder [@self [x y]])
-         {:result x} (reset! state x)
-         [:get] (println @state)))))
+      #(case %
+         :inc (swap! count inc)
+         :value @count))))
 
-(adder-client [10 20])
-(adder-client [:get])
-;;-> 30
+(counter :inc)
+(def counter-get (counter :sync))
+(counter-get :value)
+;; => 1
+(def inc-with-result (counter :async))
+(def f (inc-with-result :inc))
+(.get f)
+;; => 2
+
+;; The next example shows how to link together actors to monitor each other.
+;; The `monitor` actor will be notified whenever `client` raises an
+;; unhandled exception or when it quits.
+(defact client
+  #(case %
+     :a (throw (Exception. "blah!"))
+     :b (quit "done")
+     :name :nemo))
+
+(defact monitor
+  #(match %
+     [:exit from reason data]
+     (println ((from :sync) :name) "exited with reason" reason ", " data)))
+
+(def linker (client :linker))
+(linker :link monitor)
+(client :a)
+;; -> <error message captured and printed by monitor>
+(client :b)
+;; -> <reason for calling quit captured and printed by monitor>
+(linker :unlink monitor)
+````
+
+```clojure
+;; The internal thread pool and message queue used by an actor can be
+;; customized as shown in the following program:
+(def exec (java.util.concurrent.Executors/newFixedThreadPool 1))
+
+(defact a1
+  {:exec exec :queue (java.util.concurrent.ArrayBlockingQueue. 2)}
+  #(println %))
+
+(a1 :hello)
+;; -> :hello
+(.shutdown exec)
 ```
 
 ```clojure
-;; Shutdown the actor subsystem. 
+;; When we are done, please don't forget to shutdown the actor subsystem!
 (cljact.core/shutdown)
 ```
--- src/cljact/core.clj
+++ src/cljact/core.clj
@@ -5,13 +5,14 @@
       :author "Vijay Mathew Pandyalakal"}
   cljact.core
   (:require [clojure.core.match :refer [match]])
-  (:import [java.util.concurrent Executors ConcurrentLinkedQueue]))
+  (:import [java.util.concurrent Executors ConcurrentLinkedQueue
+            ExecutorService Callable]))
 
-(def pool (Executors/newCachedThreadPool))
+(def ^ExecutorService root-exec (Executors/newCachedThreadPool))
 
-(defmacro schedule
-  [expr]
-  `(.submit pool (fn [] ~expr)))
+(defn schedule
+  [^ExecutorService exec ^Callable f]
+  (.submit exec f))
 
 (defmacro act
   [& pat-disp]
@@ -23,18 +24,29 @@
                          (if (contains? to :loop?)
                            (:loop? to)
                            true)]))]
-    `(let [msg-q# (ConcurrentLinkedQueue.)
+    `(let [^java.util.AbstractQueue
+           msg-q# ~(if options
+                     (or (:queue options) (ConcurrentLinkedQueue.))
+                     (ConcurrentLinkedQueue.))
+           exec# ~(if options (or (:exec options) 'root-exec) 'root-exec)
+           links# (atom [])
            ~(symbol 'self) (atom nil)
+           notify-links# (fn [reason# ex#]
+                           (doseq [l# @links#]
+                             (l# [:exit @~(symbol 'self) reason# ex#])))
            running# (atom true)
-           ~(symbol 'quit) (fn [] (reset! running# false))
+           ~(symbol 'quit) (fn [& args#] (reset! running# false) (notify-links# :quit args#))
            curr-time-ms# (atom (when ~timeout-ms (System/currentTimeMillis)))
            handler# (fn [cont#]
                       (when @curr-time-ms#
                         (reset! curr-time-ms# (System/currentTimeMillis)))
                       (if-let [m# (.poll msg-q#)]
-                        (do (~@msg-handler m#)
-                            (when (and @running# (.peek msg-q#)) (schedule (cont# cont#))))
-                        (Thread/yield)))
+                        (do (try
+                              (~@msg-handler m#)
+                              (catch Exception ex#
+                                (notify-links# :error ex#)))
+                            (when (and @running# (.peek msg-q#)) (schedule exec# #(cont# cont#))))
+                          (Thread/yield)))
            to-handler# (when ~timeout-ms
                          (fn [cont#]
                            (when-let [old# @curr-time-ms#]
@@ -42,18 +54,32 @@
                                (do ~timeout-handler
                                    (when ~reschedule-timer?
                                      (reset! curr-time-ms# (System/currentTimeMillis))
-                                     (when @running# (schedule (cont# cont#)))))
-                               (when @running# (schedule (cont# cont#)))))))
-           adder# (fn [msg#]
-                    (let [schd# (.peek msg-q#)]
-                      (.add msg-q# msg#)
-                      (when-not schd#
-                        (when @running# (schedule (handler# handler#))))
-                      msg#))]
-       (reset! ~(symbol 'self) adder#)
+                                     (when @running# (schedule exec# #(cont# cont#)))))
+                               (when @running# (schedule exec# #(cont# cont#)))))))
+           act-with-future# (fn [msg#]
+                              (when @running# (schedule exec# #(~@msg-handler msg#))))
+           act-blocking# (fn [msg#]
+                           (~@msg-handler msg#))
+           linker# (fn [action# actor#]
+                     (let [r# (case action#
+                                :link (swap! links# conj actor#)
+                                :unlink (swap! links# (fn [x#] (remove #{actor#} x#))))]
+                       (if r# true false)))
+           actor# (fn [msg#]
+                    (case msg#
+                      :async act-with-future#
+                      :sync act-blocking#
+                      :linker linker#
+                      :quit (~(symbol 'quit))
+                      (let [schd# (.peek msg-q#)]
+                        (.add msg-q# msg#)
+                        (when-not schd#
+                          (when @running# (schedule exec# #(handler# handler#))))
+                        msg#)))]
+       (reset! ~(symbol 'self) actor#)
        (when to-handler#
-         (schedule (to-handler# to-handler#)))
-       adder#)))
+         (schedule exec# #(to-handler# to-handler#)))
+       actor#)))
 
 (defmacro defact
   [name & defs]
@@ -65,4 +91,4 @@
 
 (defn shutdown
   []
-  (.shutdown pool))
+  (.shutdown root-exec))
